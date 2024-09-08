6d41fc9a99be40f7f1d651049608528b045a2b99
sma-bari <mdazizulbarishafin@gmail.com>
2024-09-08 20:38:34 +0600

    added page animaiton

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const heroAnimation = () => {
     duration: 0.5,
     stagger: 0.1,
   });
-  tl.from(".left h1, .left p, .left button", {
+  tl.from(".hero .left h1,.hero .left p,.hero .left button", {
     x: -100,
     opacity: 0,
     duration: 0.5,
     stagger: 0.2,
-  });
-  tl.from(".right img", {
+  }, "hero");
+  tl.from(".hero .right img", {
     opacity: 0,
     x: -100,
-    duration: 0.2,
-  });
+    duration: 0.5,
+  }, "hero");
   tl.from(".brands img", {
     y: 100,
     opacity: 0,
@@ -26,4 +26,67 @@ const heroAnimation = () => {
   });
 };
 
+const pageAnimation = ()=>{
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".cards",
+            scroller: "body",
+            // markers: true,
+            start: "top 50%",
+            end: "top -70%",
+            scrub: 2,
+        }
+    })
+    tl.from(".services h4, .services p",{
+        y: -100,
+        opacity: 0,
+        stagger: 0.1,
+    })
+    // cards1
+    tl.from(".line1 .left",{
+        x: -100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line1")
+    tl.from(".line1 .right",{
+        x: 100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line1")
+    // cards2
+    tl.from(".line2 .left",{
+        x: -100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line2")
+    tl.from(".line2 .right",{
+        x: 100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line2")
+    // cards3
+    tl.from(".line3 .left",{
+        x: -100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line3")
+    tl.from(".line3 .right",{
+        x: 100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line3")
+    // cards4
+    tl.from(".line4 .left",{
+        x: -100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line4")
+    tl.from(".line4 .right",{
+        x: 100,
+        opacity: 0,
+        duration: 0.5,
+    }, "line4")
+}
+
 heroAnimation()
+pageAnimation()
